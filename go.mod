eaf56041430fcff16860313e5a5f534151ee73db
Marc Denning <marc.denning@gmail.com>
2020-04-13 20:18:56 -0400

    Add Google Cloud Storage client library.
    Iterate objects in configured bucket during server startup.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module media-browser
 
 go 1.14
+
+require (
+	cloud.google.com/go/storage v1.6.0
+	google.golang.org/api v0.18.0
+)
--- media-browser.go
+++ media-browser.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"cloud.google.com/go/storage"
+	"context"
 	"fmt"
+	"google.golang.org/api/iterator"
 	"log"
 	"net/http"
 	"os"
@@ -24,6 +27,29 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
+	}
+
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bkt := client.Bucket(os.Getenv("BUCKET_NAME"))
+
+	query := &storage.Query{Prefix: ""}
+
+	var names []string
+	it := bkt.Objects(ctx, query)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		names = append(names, attrs.Name)
+		log.Println(fmt.Sprintf("File: %s\n", attrs.Name))
 	}
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
